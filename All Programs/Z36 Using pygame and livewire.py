eff7544ff43d92da8dc514ed2499398ebdffae48
Oreoluwa-IVT <oreoluwaosewe.oluwafemi@stu.cu.edu.ng>
2022-01-03 20:30:09 +0100

    USing pygame

---
+++ All Programs/Z36 Using pygame and livewire.py
@@ -0,0 +1,6 @@
+#This packages allow you to exploit the power of Pythong graphics 
+#livewire is a package which contains many modules. 
+# In this program ,we'll be importing a few modules from that package
+
+
+from livewires import games
